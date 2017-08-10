3bc453498833f79235b2b5ea17eecefa230d03cd
jakecronin <jake.cronin4@gmail.com>
2017-08-09 23:02:33 -0400

    added global variables

---
+++ MyWeb/GlobalVariables.swift
@@ -0,0 +1,13 @@
+//
+//  GlobalVariables.swift
+//  MyWeb
+//
+//  Created by Jake Cronin on 8/9/17.
+//  Copyright © 2017 Jake Cronin. All rights reserved.
+//
+
+import Foundation
+
+
+var admobAppID = "ca-app-pub-7240573263963478~3305371499"
+var admobBannerID =  "ca-app-pub-7240573263963478/9143009779"
